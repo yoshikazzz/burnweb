d4a28c42b00d6a5b73e0ab7b13733fd704a280c8
Toru Nayuki <nayuki@nayuki-at-work.net>
2020-11-17 13:08:58 +0900

    Lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import axios, { AxiosInstance } from 'axios'
-import abi from 'ethereumjs-abi'
+import axios, { AxiosInstance } from 'axios';
+import abi from 'ethereumjs-abi';
 import Common from 'ethereumjs-common';
-import { Transaction } from 'ethereumjs-tx'
+import { Transaction } from 'ethereumjs-tx';
 import Wallet from 'ethereumjs-wallet';
 
 type BlockNumber = string | number;
@@ -15,7 +15,7 @@ class BurnWeb {
 
     private async getCustomCommon(): Promise<Common> {
         if (this._customCommon === null) {
-            const blockchain = (await this.axios.get('api/blockchain')).data
+            const blockchain = (await this.axios.get('api/blockchain')).data;
 
             this._customCommon = Common.forCustomChain(
                 'mainnet',
@@ -25,68 +25,68 @@ class BurnWeb {
                     chainId: blockchain['chain_id'],
                 },
                 'petersburg'
-            )
+            );
         }
 
-        return this._customCommon
+        return this._customCommon;
     }
 
     constructor(url: string, privateKey: string | undefined) {
         this.axios = axios.create({
             baseURL: url,
             responseType: 'json'
-        })
+        });
 
         this.axios.interceptors.response.use(function(response) {
-            return response
+            return response;
         }, function(error) {
-            console.log(error.response)
+            console.log(error.response);
             if (error.response.status == 500) {
-                return Promise.reject(new Error(error.response.data.message))
+                return Promise.reject(new Error(error.response.data.message));
             }
 
-            return Promise.reject(error)
-        })
+            return Promise.reject(error);
+        });
 
         if (privateKey !== undefined) {
-            this.privateKey = Buffer.from(privateKey, 'hex')
+            this.privateKey = Buffer.from(privateKey, 'hex');
         }
     }
 
     getBlockNumber(): Promise<number> {
         return this.axios.get('api/block_number').then((response) => {
-            return response.data['block_number']
-        })
+            return response.data['block_number'];
+        });
     }
 
     getBlock(blockHashOrBlockNumber: BlockNumber | string): Promise<number> {
         return this.axios.get('api/blocks/' + blockHashOrBlockNumber).then((response) => {
-            return response.data
-        })
+            return response.data;
+        });
     }
 
     getBalance(address: string): Promise<string> {
         return this.axios.get('api/token/0x0000000000000000000000000000000000000000/balance/' + address).then((response) => {
-            return response.data['balance']
-        })
+            return response.data['balance'];
+        });
     }
 
     getBalanceOf(tokenId: string, address: string): Promise<string> {
         return this.axios.get('api/token/' + tokenId +'/balance/' + address).then((response) => {
-            return response.data['balance']
-        })
+            return response.data['balance'];
+        });
     }
 
     getTransaction(transactionHash: string): Promise<object> {
         return this.axios.get('api/transactions/' + transactionHash).then((response) => {
-            return response.data
-        })
+            return response.data;
+        });
     }
 
     getToken(tokenId: string): Promise<object> {
         return this.axios.get('api/token/' + tokenId).then((response) => {
-            return response.data
-        })
+            return response.data;
+        });
     }
 
     async createToken(
@@ -115,7 +115,7 @@ class BurnWeb {
                 mintable,
                 burnable
             ]
-        )
+        );
 
         const tokenId = Wallet.generate().getAddressString();
         const nonce = Date.now();
@@ -127,9 +127,9 @@ class BurnWeb {
             to: tokenId,
             value: '0x0',
             data: '0x78e2fc09' + data.toString('hex')
-        }
+        };
 
-        const tx = new Transaction(txParams, { common: await this.getCustomCommon() })
+        const tx = new Transaction(txParams, { common: await this.getCustomCommon() });
   
         tx.sign(this.privateKey);
   
@@ -138,17 +138,17 @@ class BurnWeb {
         return this.axios.post('api/token/create', {
             'token_id': tokenId,
             'name': name,
-            "symbol": symbol,
-            "decimals": decimals,
-            "total_supply": totalSupply,
-            "fee_token_id": feeTokenId,
-            "tx_fee": txFee,
-            "tx_fee_rate": txFeeRate,
-            "icon": icon,
-            "mintable": mintable,
-            "burnable": burnable,
-            "nonce": nonce,
-            "signature": signature
+            'symbol': symbol,
+            'decimals': decimals,
+            'total_supply': totalSupply,
+            'fee_token_id': feeTokenId,
+            'tx_fee': txFee,
+            'tx_fee_rate': txFeeRate,
+            'icon': icon,
+            'mintable': mintable,
+            'burnable': burnable,
+            'nonce': nonce,
+            'signature': signature
         }).then((response) => {
             return {
                 txHash: response.data['tx_hash'],
@@ -162,7 +162,7 @@ class BurnWeb {
         target: string,
         amount: number
     ): Promise<string> {
-        const data = abi.rawEncode([ 'address', 'uint256' ], [ target, amount ])
+        const data = abi.rawEncode([ 'address', 'uint256' ], [ target, amount ]);
         const nonce = Date.now();
 
         const txParams = {
@@ -172,9 +172,9 @@ class BurnWeb {
             to: tokenId,
             value: '0x0',
             data: '0xa9059cbb' + data.toString('hex')
-        }
+        };
 
-        const tx = new Transaction(txParams, { common: await this.getCustomCommon() })
+        const tx = new Transaction(txParams, { common: await this.getCustomCommon() });
   
         tx.sign(this.privateKey);
   
@@ -186,7 +186,7 @@ class BurnWeb {
             'nonce': nonce,
             'signature': signature
         }).then((response) => {
-            return response.data['tx_hash']
+            return response.data['tx_hash'];
         });
     }
 
@@ -195,7 +195,7 @@ class BurnWeb {
         target: string,
         amount: number
     ): Promise<string> {
-        const data = abi.rawEncode([ 'address', 'uint256' ], [ target, amount ])
+        const data = abi.rawEncode([ 'address', 'uint256' ], [ target, amount ]);
         const nonce = Date.now();
 
         const txParams = {
@@ -205,9 +205,9 @@ class BurnWeb {
             to: tokenId,
             value: '0x0',
             data: '0x40c10f19' + data.toString('hex')
-        }
+        };
 
-        const tx = new Transaction(txParams, { common: await this.getCustomCommon() })
+        const tx = new Transaction(txParams, { common: await this.getCustomCommon() });
   
         tx.sign(this.privateKey);
   
@@ -219,7 +219,7 @@ class BurnWeb {
             'nonce': nonce,
             'signature': signature
         }).then((response) => {
-            return response.data['tx_hash']
+            return response.data['tx_hash'];
         });
     }
 
@@ -227,7 +227,7 @@ class BurnWeb {
         tokenId: string,
         amount: number
     ): Promise<string> {
-        const data = abi.rawEncode([ 'uint256' ], [ amount ])
+        const data = abi.rawEncode([ 'uint256' ], [ amount ]);
         const nonce = Date.now();
 
         const txParams = {
@@ -237,9 +237,9 @@ class BurnWeb {
             to: tokenId,
             value: '0x0',
             data: '0x42966c68' + data.toString('hex')
-        }
+        };
 
-        const tx = new Transaction(txParams, { common: await this.getCustomCommon() })
+        const tx = new Transaction(txParams, { common: await this.getCustomCommon() });
   
         tx.sign(this.privateKey);
   
@@ -250,7 +250,7 @@ class BurnWeb {
             'nonce': nonce,
             'signature': signature
         }).then((response) => {
-            return response.data['tx_hash']
+            return response.data['tx_hash'];
         });
     }
 }
