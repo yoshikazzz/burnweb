ed6bdd90fd1acb559efc94b959713f81320ecf6f
BinhND <binhnd@socicom.com.vn>
2020-11-24 06:32:49 +0700

    Update ts config to support js

---
+++ dist/index.d.ts
@@ -0,0 +1,33 @@
+declare type BlockNumber = string | number;
+declare class BurnWeb {
+    private readonly axios;
+    private readonly privateKey;
+    private _customCommon;
+    private getCustomCommon;
+    constructor(url: string, privateKey: string | undefined);
+    getBlockNumber(): Promise<number>;
+    getBlock(blockHashOrBlockNumber: BlockNumber | string): Promise<number>;
+    getBalance(address: string): Promise<string>;
+    getBalanceOf(tokenId: string, address: string): Promise<string>;
+    getTransaction(transactionHash: string): Promise<object>;
+    getToken(tokenId: string): Promise<object>;
+    createToken(name: string, symbol: string, decimals: number, totalSupply: string, feeTokenId: string, txFee: number, txFeeRate: number, icon: string, mintable: boolean, burnable: boolean): Promise<{
+        txHash: string;
+        tokenId: string;
+    }>;
+    transferToken(tokenId: string, target: string, amount: number): Promise<string>;
+    issueToken(tokenId: string, target: string, amount: number): Promise<string>;
+    burnToken(tokenId: string, amount: number): Promise<string>;
+    createStore(name: string): Promise<{
+        txHash: string;
+        storeId: string;
+    }>;
+    setKeyValue(storeId: string, collection: string, key: string, value: string): Promise<{
+        txHash: string;
+    }>;
+    getKeyValue(storeId: string, collection: string, key: string): Promise<string>;
+    deleteKeyValue(storeId: string, collection: string, key: string): Promise<{
+        txHash: string;
+    }>;
+}
+export = BurnWeb;
---
+++ dist/index.js
@@ -0,0 +1,302 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const axios_1 = __importDefault(require("axios"));
+const ethereumjs_abi_1 = __importDefault(require("ethereumjs-abi"));
+const ethereumjs_common_1 = __importDefault(require("ethereumjs-common"));
+const ethereumjs_tx_1 = require("ethereumjs-tx");
+const ethereumjs_wallet_1 = __importDefault(require("ethereumjs-wallet"));
+class BurnWeb {
+    constructor(url, privateKey) {
+        this._customCommon = null;
+        this.axios = axios_1.default.create({
+            baseURL: url,
+            responseType: 'json'
+        });
+        this.axios.interceptors.response.use(function (response) {
+            return response;
+        }, function (error) {
+            console.log(error.response);
+            if (error.response.status == 500) {
+                return Promise.reject(new Error(error.response.data.message));
+            }
+            return Promise.reject(error);
+        });
+        if (privateKey !== undefined) {
+            this.privateKey = Buffer.from(privateKey, 'hex');
+        }
+    }
+    getCustomCommon() {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (this._customCommon === null) {
+                const blockchain = (yield this.axios.get('api/blockchain')).data;
+                this._customCommon = ethereumjs_common_1.default.forCustomChain('mainnet', {
+                    name: 'BURN',
+                    networkId: blockchain['chain_id'],
+                    chainId: blockchain['chain_id'],
+                }, 'petersburg');
+            }
+            return this._customCommon;
+        });
+    }
+    getBlockNumber() {
+        return this.axios.get('api/block_number').then((response) => {
+            return response.data['block_number'];
+        });
+    }
+    getBlock(blockHashOrBlockNumber) {
+        return this.axios.get('api/blocks/' + blockHashOrBlockNumber).then((response) => {
+            return response.data;
+        });
+    }
+    getBalance(address) {
+        return this.axios.get('api/token/0x0000000000000000000000000000000000000000/balance/' + address).then((response) => {
+            return response.data['balance'];
+        });
+    }
+    getBalanceOf(tokenId, address) {
+        return this.axios.get('api/token/' + tokenId + '/balance/' + address).then((response) => {
+            return response.data['balance'];
+        });
+    }
+    getTransaction(transactionHash) {
+        return this.axios.get('api/transactions/' + transactionHash).then((response) => {
+            return response.data;
+        });
+    }
+    getToken(tokenId) {
+        return this.axios.get('api/token/' + tokenId).then((response) => {
+            return response.data;
+        });
+    }
+    createToken(name, symbol, decimals, totalSupply, feeTokenId, txFee, txFeeRate, icon, mintable, burnable) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const data = ethereumjs_abi_1.default.rawEncode(['string', 'string', 'uint256', 'uint256', 'address', 'uint256', 'uint256', 'string', 'uint256', 'uint256'], [
+                name,
+                symbol,
+                decimals,
+                totalSupply,
+                feeTokenId,
+                txFee,
+                txFeeRate,
+                icon,
+                mintable,
+                burnable
+            ]);
+            const tokenId = ethereumjs_wallet_1.default.generate().getAddressString();
+            const nonce = Date.now();
+            const txParams = {
+                nonce: '0x' + nonce.toString(16),
+                gasPrice: '0x0',
+                gasLimit: '0x0',
+                to: tokenId,
+                value: '0x0',
+                data: '0x78e2fc09' + data.toString('hex')
+            };
+            const tx = new ethereumjs_tx_1.Transaction(txParams, { common: yield this.getCustomCommon() });
+            tx.sign(this.privateKey);
+            const signature = tx.v.toString('hex') + tx.r.toString('hex') + tx.s.toString('hex');
+            return this.axios.post('api/token/create', {
+                'token_id': tokenId,
+                'name': name,
+                'symbol': symbol,
+                'decimals': decimals,
+                'total_supply': totalSupply,
+                'fee_token_id': feeTokenId,
+                'tx_fee': txFee,
+                'tx_fee_rate': txFeeRate,
+                'icon': icon,
+                'mintable': mintable,
+                'burnable': burnable,
+                'nonce': nonce,
+                'signature': signature
+            }).then((response) => {
+                return {
+                    txHash: response.data['tx_hash'],
+                    tokenId: tokenId
+                };
+            });
+        });
+    }
+    transferToken(tokenId, target, amount) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const data = ethereumjs_abi_1.default.rawEncode(['address', 'uint256'], [target, amount]);
+            const nonce = Date.now();
+            const txParams = {
+                nonce: '0x' + nonce.toString(16),
+                gasPrice: '0x0',
+                gasLimit: '0x0',
+                to: tokenId,
+                value: '0x0',
+                data: '0xa9059cbb' + data.toString('hex')
+            };
+            const tx = new ethereumjs_tx_1.Transaction(txParams, { common: yield this.getCustomCommon() });
+            tx.sign(this.privateKey);
+            const signature = tx.v.toString('hex') + tx.r.toString('hex') + tx.s.toString('hex');
+            return this.axios.post('api/token/' + tokenId + '/transfer', {
+                'to': target,
+                'value': amount,
+                'nonce': nonce,
+                'signature': signature
+            }).then((response) => {
+                return response.data['tx_hash'];
+            });
+        });
+    }
+    issueToken(tokenId, target, amount) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const data = ethereumjs_abi_1.default.rawEncode(['address', 'uint256'], [target, amount]);
+            const nonce = Date.now();
+            const txParams = {
+                nonce: '0x' + nonce.toString(16),
+                gasPrice: '0x0',
+                gasLimit: '0x0',
+                to: tokenId,
+                value: '0x0',
+                data: '0x40c10f19' + data.toString('hex')
+            };
+            const tx = new ethereumjs_tx_1.Transaction(txParams, { common: yield this.getCustomCommon() });
+            tx.sign(this.privateKey);
+            const signature = tx.v.toString('hex') + tx.r.toString('hex') + tx.s.toString('hex');
+            return this.axios.post('api/token/' + tokenId + '/issue', {
+                'to': target,
+                'value': amount,
+                'nonce': nonce,
+                'signature': signature
+            }).then((response) => {
+                return response.data['tx_hash'];
+            });
+        });
+    }
+    burnToken(tokenId, amount) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const data = ethereumjs_abi_1.default.rawEncode(['uint256'], [amount]);
+            const nonce = Date.now();
+            const txParams = {
+                nonce: '0x' + nonce.toString(16),
+                gasPrice: '0x0',
+                gasLimit: '0x0',
+                to: tokenId,
+                value: '0x0',
+                data: '0x42966c68' + data.toString('hex')
+            };
+            const tx = new ethereumjs_tx_1.Transaction(txParams, { common: yield this.getCustomCommon() });
+            tx.sign(this.privateKey);
+            const signature = tx.v.toString('hex') + tx.r.toString('hex') + tx.s.toString('hex');
+            return this.axios.post('api/token/' + tokenId + '/burn', {
+                'value': amount,
+                'nonce': nonce,
+                'signature': signature
+            }).then((response) => {
+                return response.data['tx_hash'];
+            });
+        });
+    }
+    createStore(name) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const data = ethereumjs_abi_1.default.rawEncode(['string', 'address', 'uint256'], [name, '0x0000000000000000000000000000000000000000', '0']);
+            const storeId = ethereumjs_wallet_1.default.generate().getAddressString();
+            const nonce = Date.now();
+            const txParams = {
+                nonce: '0x' + nonce.toString(16),
+                gasPrice: '0x0',
+                gasLimit: '0x0',
+                to: storeId,
+                value: '0x0',
+                data: '0x06dc8de5' + data.toString('hex')
+            };
+            const tx = new ethereumjs_tx_1.Transaction(txParams, { common: yield this.getCustomCommon() });
+            tx.sign(this.privateKey);
+            const signature = tx.v.toString('hex') + tx.r.toString('hex') + tx.s.toString('hex');
+            return this.axios.post('api/kvs/create', {
+                'store_id': storeId,
+                'name': name,
+                'fee_token_id': '0x0000000000000000000000000000000000000000',
+                'tx_fee': 0,
+                'nonce': nonce,
+                'signature': signature
+            }).then((response) => {
+                return {
+                    txHash: response.data['tx_hash'],
+                    storeId: storeId
+                };
+            });
+        });
+    }
+    setKeyValue(storeId, collection, key, value) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const data = ethereumjs_abi_1.default.rawEncode(['string', 'string', 'string'], [collection, key, value]);
+            const nonce = Date.now();
+            const txParams = {
+                nonce: '0x' + nonce.toString(16),
+                gasPrice: '0x0',
+                gasLimit: '0x0',
+                to: storeId,
+                value: '0x0',
+                data: '0xc6be612d' + data.toString('hex')
+            };
+            const tx = new ethereumjs_tx_1.Transaction(txParams, { common: yield this.getCustomCommon() });
+            tx.sign(this.privateKey);
+            const signature = tx.v.toString('hex') + tx.r.toString('hex') + tx.s.toString('hex');
+            return this.axios.post('api/kvs/' + storeId + '/collections/' + collection, {
+                'store_id': storeId,
+                'key': key,
+                'value': value,
+                'nonce': nonce,
+                'signature': signature
+            }).then((response) => {
+                return {
+                    txHash: response.data['tx_hash']
+                };
+            });
+        });
+    }
+    getKeyValue(storeId, collection, key) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.axios.get('api/kvs/' + storeId + '/collections/' + collection + '/keys/' + key + '/value').then((response) => {
+                return response.data['value'];
+            });
+        });
+    }
+    deleteKeyValue(storeId, collection, key) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const data = ethereumjs_abi_1.default.rawEncode(['string', 'string'], [collection, key]);
+            const nonce = Date.now();
+            const txParams = {
+                nonce: '0x' + nonce.toString(16),
+                gasPrice: '0x0',
+                gasLimit: '0x0',
+                to: storeId,
+                value: '0x0',
+                data: '0x668f1f1b' + data.toString('hex')
+            };
+            const tx = new ethereumjs_tx_1.Transaction(txParams, { common: yield this.getCustomCommon() });
+            tx.sign(this.privateKey);
+            const signature = tx.v.toString('hex') + tx.r.toString('hex') + tx.s.toString('hex');
+            return this.axios.delete('api/kvs/' + storeId + '/collections/' + collection, {
+                data: {
+                    'store_id': storeId,
+                    'key': key,
+                    'nonce': nonce,
+                    'signature': signature
+                }
+            }).then((response) => {
+                return {
+                    txHash: response.data['tx_hash']
+                };
+            });
+        });
+    }
+}
+module.exports = BurnWeb;
+//# sourceMappingURL=index.js.map
